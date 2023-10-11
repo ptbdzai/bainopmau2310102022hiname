d030f7d58e299e612c49a1cf3962a75c8bf1e6fa
ptbdzai <147029992+ptbdzai@users.noreply.github.com>
2023-10-11 12:02:01 +0000

    Nopbai

--- s00_bailam.py
+++ s00_bailam.py
@@ -20,8 +20,10 @@ Hay viet ham hi(name) xuat ra cau chao theo mota benduoi
 Khi chay voi input           | Ketqua output
 ---------------------------- | -----------------
 hi('Mom')                    | Hi Mom!
+hi('')                       | Hi!
 hi()                         | Hi!
 hi(None)                     | Hi!
+hi(name='Mom')               | Hi Mom!
 
 ------------------- mucdo: kho -----------------
 hi('Mom', 'Dad')             | Hi Mom, and Dad!
@@ -31,9 +33,34 @@ hi('1', '22', '333', '4444') | Hi A, B, and C!
 #endregion debai
 
 #region bailam
-def hi(name):
-  return 'todo'
+#             **kwargs to allow calling hi(name='xx') without error > TypeError: hi() got an unexpected keyword argument 'name'
+def hi(*args, **kwargs):
+  a=[None]*100
+  nam=kwargs.get('name')
+  tf=False
+  i=-1
+  if len(args)!=0:
+    for i in range(len(args)):
+      a[i]=args[i]
 
-# def hi(args, kwargs):
-#   return 'todo'
+  if nam!=None:
+      i+=1
+      a[i]=nam
+      
+  if i==-1:
+    return 'Hi!'
+    
+  elif i==0:
+    return 'Hi '+a[i]+'!'
+    
+  else:
+    p=0
+    s='Hi'
+    for j in range(i+1):
+      if j!=i:
+        s=f"{s} {a[j]},"
+      else:
+        s=f"{s} and {a[j]}!"
+    return s
+  
 #endregion bailam
